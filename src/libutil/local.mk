d147bfcf17b2e88549bc009101be1c15a1b4cf88
Bryan Lunt <guitarbryan@gmail.com>
2019-01-05 19:11:22 -0600

    Actually compiles on FreeBSD
    Need this to compile on FreeBSD.

--- src/libutil/local.mk
+++ src/libutil/local.mk
@@ -6,7 +6,9 @@ libutil_DIR := $(d)
 
 libutil_SOURCES := $(wildcard $(d)/*.cc)
 
-libutil_LDFLAGS = $(LIBLZMA_LIBS) -lbz2 -pthread $(OPENSSL_LIBS) $(LIBBROTLI_LIBS) -lboost_context -lc
+libutil_ALLOW_UNDEFINED = true
+
+libutil_LDFLAGS = $(LIBLZMA_LIBS) -lbz2 -pthread $(OPENSSL_LIBS) $(LIBBROTLI_LIBS) -lboost_context
 
 libutil_LIBS = libformat
 
